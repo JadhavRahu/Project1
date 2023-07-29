7e84d7e579a0b0573dbbc2f1c9baa0a2a3f4df3a
JadhavRahu <135210026+JadhavRahu@users.noreply.github.com>
2023-07-29 15:44:50 +0000

    Age Calculator

---
+++ assignment06(Age).py
@@ -0,0 +1,21 @@
+#Calculating of Age
+day1 = int(input('Enter birth day:'))
+month1 = int(input('Enter birth month:'))
+year1 = int(input('Enter birth year:'))
+
+Currentday2 = int(input('Enter Currentday :'))
+Currentmonth2 = int(input('Enter Currentmonth :'))
+Currentyear2 = int(input('Enter Currentyear:'))
+
+print("Enter birth day : ",day1)
+print("Enter birth month : ",month1)
+print("Enter birth year : ",year1)
+
+
+print("Enter birth day : ",Currentday2)
+print("Enter birth month : ",Currentmonth2)
+print("Enter birth year : ",Currentyear2)
+
+
+
+
